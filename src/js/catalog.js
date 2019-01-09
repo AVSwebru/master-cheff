1933a29ac683ec4dd23a28c8ff23e234c525cadf
remzov <remzovnik@gmail.com>
2019-01-09 16:37:52 +0300

    correction

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,20 +1,21 @@
 export default (catalogElements) => {
-   
+
     const {catalog, header, catalogBar, catalogOpenBtn, catalogCloseBtn} = catalogElements;
 
     catalog.style.top = header.getBoundingClientRect().height + catalogBar.getBoundingClientRect() + 'px';
-  
+
     catalogOpenBtn.addEventListener('click', () => {
+        console.log('test')
         if (catalog.hidden === true) {
-            catalog.hidden = false; 
+            catalog.hidden = false;
             catalog.classList.add('uk-animation-slide-top');
         } else {
             catalog.hidden = true;
-        } 
+        }
     });
 
     catalogCloseBtn.addEventListener('click', () => {
-        catalog.hidden = true;          
+        catalog.hidden = true;
     });
 
 };
